35a491064eeb664632d8dbc950d78d24d9e42951
Fenuku kekeli <113697135+Kekelifenuku@users.noreply.github.com>
2022-11-26 00:13:00 +0000

    Add files via upload

---
+++ Onbording/Onbording/OnbordingApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnbordingApp.swift
+//  Onbording
+//
+//  Created by kekeli on 25/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct OnbordingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
